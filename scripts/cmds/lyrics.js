92fb82cce0c4e4d374475843acccad5fe2827759
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-07-18 19:10:10 +0300

    ༆✞

--- scripts/cmds/lyrics.js
+++ scripts/cmds/lyrics.js
@@ -19,7 +19,7 @@ module.exports = {
         const searchMessage = await bot.sendMessage(chatId, `🔍 Searching for lyrics: ${query}`);
 
         try {
-            const response = await axios.get(`https://apis-samir.onrender.com/lyrics?query=${encodeURIComponent(query)}`);
+            const response = await axios.get(`https://samirxpikachuio.onrender.com/lyrics?query=${encodeURIComponent(query)}`);
             const { title, artist, lyrics, image } = response.data;
 
             await bot.sendMessage(chatId, `Lyrics: ${lyrics}\n\nSong Name: ${title}\n\nWriter: ${artist}`);
