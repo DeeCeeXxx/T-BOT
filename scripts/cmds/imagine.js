b89179238493234362f793af334b6b941a2bc957
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-08-16 15:42:51 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- scripts/cmds/imagine.js
+++ scripts/cmds/imagine.js
@@ -18,7 +18,7 @@ module.exports = {
         }
 
         try {
-            const apiUrl = `https://keith-api.onrender.com/imagine?prompt=${encodeURIComponent(prompt)}`;
+            const apiUrl = `https://samirxpikachuio.onrender.com/imagine?prompt=${encodeURIComponent(prompt)}`;
             const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
             const imageData = Buffer.from(response.data, 'binary');
             await bot.sendPhoto(chatId, imageData);
