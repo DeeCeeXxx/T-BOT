feaf06f3bd84e12d8052f790ac37ac6a49b8f788
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-08-16 14:36:08 +0300

    Update weather.js

--- scripts/cmds/weather.js
+++ scripts/cmds/weather.js
@@ -20,7 +20,7 @@ module.exports = {
         }
 
         try {
-            const apiUrl = `https://apis-samir.onrender.com/weather/${encodeURIComponent(city)}`;
+            const apiUrl = `https://samirxpikachuio.onrender.com/weather/${encodeURIComponent(city)}`;
             const response = await axios.get(apiUrl);
             const weatherData = response.data;
 
